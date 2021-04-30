077b5b356fe337d37d036d6c68a2fc8511baa09e
brlga002 <gabriel.lima1000@gmail.com>
2021-04-30 16:47:43 -0400

    feature: add build js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-require('./bootstrap');
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import PagesRoute from "./pages/Routes";
+import "../css/styles.css";
+import "../css/global.css";
+
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <PagesRoute />
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("app"));
