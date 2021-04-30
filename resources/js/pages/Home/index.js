036230a1333c08ced5b41104d6d6bf060e31d0ae
brlga002 <gabriel.lima1000@gmail.com>
2021-04-30 16:47:07 -0400

    feature: add initial pages

---
+++ resources/js/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// import { Container } from "./styles";
+import ThemePage from "../../components/ThemePage";
+
+function Home() {
+    return <ThemePage>Home</ThemePage>;
+}
+
+export default Home;
