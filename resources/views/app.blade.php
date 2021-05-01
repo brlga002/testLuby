7aac6b5d4d8cdfbb0d54ec490cfe5d4f3c26fc48
brlga002 <gabriel.lima1000@gmail.com>
2021-04-30 22:04:17 -0400

    fix: indent

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
-    <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>LUBY - Acadêmico</title>
-                
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900">
-    </head>
-    <body>
-        <div id="app"></div>
-        <script src="{{ asset('js/app.js') }}"></script>
-    </body>
-    </html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>LUBY - Acadêmico</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900">
+</head>
+
+<body>
+    <div id="app"></div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
