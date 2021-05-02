14905fe11757aa5d2acff1544288f0bd5ff1ce8c
brlga002 <gabriel.lima1000@gmail.com>
2021-05-01 21:01:50 -0400

    feature: add styles new studant

---
+++ resources/js/pages/NewStudent/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 25px;
+  margin-bottom: 20px;
+`;
+
+export const ContainerIcon = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+
+  > * {
+    cursor: pointer;
+    border-radius: 10px;
+  }
+`;
