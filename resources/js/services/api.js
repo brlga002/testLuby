480ae77857219e9d11c632b544cb313de8562963
brlga002 <gabriel.lima1000@gmail.com>
2021-05-01 20:59:47 -0400

    feature: add service

---
+++ resources/js/services/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const headers = async () => {
+  const token = localStorage.getItem("token");
+  return { Authorization: `Bearer ${token}` };
+};
+
+const url = "http://localhost:7777/api";
+
+const api = axios.create({
+  baseURL: url,
+});
+
+api.interceptors.request.use(
+  async (config) => {
+    config.headers = await headers();
+    return config;
+  },
+
+  (error) => Promise.reject(error)
+);
+
+export default api;
