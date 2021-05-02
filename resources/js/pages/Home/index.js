b0f2bf4e5dd9b4e92780fd0a1908a3c775f39089
brlga002 <gabriel.lima1000@gmail.com>
2021-05-02 03:05:49 -0400

    feature: add data from api

--- resources/js/pages/Home/index.js
+++ resources/js/pages/Home/index.js
@@ -5,32 +5,14 @@ import { FiFilter } from "react-icons/fi";
 import { Title, ContainerIcon, Form } from "./styles";
 import ThemePage from "../../components/ThemePage";
 import { colors } from "../../theme";
+import api from "../../services/api";
+import Alert from "../../components/Alert";
 
 function Home() {
-  const students = [
-    {
-      id: 1,
-      name: "miguel fernandes",
-      nMatricula: "1235fg",
-      serie: 4,
-      sexo: "masculino",
-      idade: 5,
-      cpf: "002.130.832-21",
-      telefone: "92 99175-5655",
-    },
-    {
-      id: 2,
-      name: "gabriel fernandes",
-      nMatricula: "456er7",
-      serie: 5,
-      sexo: "feminino",
-      idade: 10,
-      cpf: "001.130.832-21",
-      telefone: "92 88175-5655",
-    },
-  ];
+  const [mensage, setMensage] = React.useState({});
   const [searchString, setSearchString] = React.useState("");
-  const [filteData, setFilteData] = React.useState(students);
+  const [students, setStudents] = React.useState([]);
+  const [filteData, setFilteData] = React.useState([]);
 
   function filter() {
     const number = searchString.length;
@@ -47,6 +29,34 @@ function Home() {
     setFilteData(newData);
   }
 
+  const loadStudents = async () => {
+    try {
+      const response = await api.get(`/students`);
+      const { data } = response;
+      setStudents(data.students);
+      setFilteData(data.students);
+    } catch (error) {
+      console.log("erros");
+    }
+  };
+
+  const deleteStudent = async (id) => {
+    try {
+      const response = await api.delete(`/students/${id}`);
+      const { data } = response;
+      const { message } = data;
+      setMensage({ type: "success", text: message });
+      loadStudents();
+    } catch (error) {
+      const message = error.response.message || "Erro ao deletar";
+      setMensage({ type: "danger", text: message });
+    }
+  };
+
+  React.useEffect(() => {
+    loadStudents();
+  }, []);
+
   React.useEffect(() => {
     filter();
   }, [searchString]);
@@ -54,7 +64,7 @@ function Home() {
   return (
     <ThemePage>
       <Title>Listagem de alunos</Title>
-
+      <Alert mensage={mensage} setMensage={setMensage} />
       <div className="table-responsive">
         <caption>
           <Form>
@@ -82,29 +92,43 @@ function Home() {
               <th>Idade</th>
               <th>CPF</th>
               <th>Telefone</th>
+              <th>Nota</th>
+              <th>Resultado</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
-            {filteData.map((student) => (
-              <tr>
-                <th>{student.id}</th>
-                <th>{student.name}</th>
-                <th>{student.nMatricula}</th>
-                <th>{student.serie}</th>
-                <th>{student.sexo}</th>
-                <th>{student.idade}</th>
-                <th>{student.cpf}</th>
-                <th>{student.telefone}</th>
-                <th>
-                  <ContainerIcon>
-                    <IoCalendarOutline size={25} color={colors.primary} />
-                    <IoCreateOutline size={25} color={colors.primary} />
-                    <IoTrash size={25} color={colors.danger} />
-                  </ContainerIcon>
-                </th>
-              </tr>
-            ))}
+            {filteData.map((student) => {
+              const { nota, resultado } = student.note || "";
+
+              return (
+                <tr>
+                  <th>{student.id}</th>
+                  <th>{student.name}</th>
+                  <th>{student.nMatricula}</th>
+                  <th>{student.serie}</th>
+                  <th>{student.sexo}</th>
+                  <th>{student.idade}</th>
+                  <th>{student.cpf}</th>
+                  <th>{student.telefone}</th>
+                  <th>{nota}</th>
+                  <th>{resultado}</th>
+                  <th>
+                    <ContainerIcon>
+                      <IoCalendarOutline size={25} color={colors.primary} />
+                      <IoCreateOutline size={25} color={colors.primary} />
+                      <IoTrash
+                        size={25}
+                        color={colors.danger}
+                        onClick={() => {
+                          deleteStudent(student.id);
+                        }}
+                      />
+                    </ContainerIcon>
+                  </th>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
