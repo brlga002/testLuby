72a1fb2b743ae45829dad41a36e90932a61b943e
brlga002 <gabriel.lima1000@gmail.com>
2021-05-01 21:41:21 -0400

    feature: add migrations

---
+++ database/migrations/2021_05_02_011140_create_notes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('nota');
+            $table->string('resultado');
+            $table->unsignedBigInteger('student_id')->unique();
+            $table->timestamps();
+
+            $table->foreign('student_id')->references('id')->on('students');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
