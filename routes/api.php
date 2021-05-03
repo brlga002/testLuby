d1e9b8020befd2d5961f8d5a14291612c8713908
brlga002 <gabriel.lima1000@gmail.com>
2021-05-02 20:00:35 -0400

    feature: add middleware token auth

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\NotesController;
 use App\Http\Controllers\StudentsController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/students', StudentsController::class);
-Route::apiResource('/students.notes', NotesController::class)->shallow();
+Route::get('/tokenNoProvider', function (Request $request) {
+    return response()->json(['message' => 'Token de acesso necessario'], 401);
+})->name('login');
+
+Route::apiResource('/students', StudentsController::class)->middleware('auth:sanctum');
+Route::apiResource('/students.notes', NotesController::class)->shallow()->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
