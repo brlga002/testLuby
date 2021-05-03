f50c7d96dfa63d35e896811f9df78982a06aeb89
brlga002 <gabriel.lima1000@gmail.com>
2021-05-02 21:10:00 -0400

    feature: add Provider store

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import PagesRoute from "./pages/Routes";
@@ -6,13 +8,15 @@ import "../css/styles.css";
 import "../css/global.css";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <PagesRoute />
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <BrowserRouter>
+          <PagesRoute />
+        </BrowserRouter>
+      </Provider>
+    </div>
+  );
 }
 
 export default App;
